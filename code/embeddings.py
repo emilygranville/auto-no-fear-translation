2816b6f4a37659067f88fd3090a64fedc7e71c44
Emily <efg12103@gmail.com>
2024-04-21 16:52:51 -0400

    cleans up the pre-trained embeddings and makes code that accesses the embeddings

---
+++ code/embeddings.py
@@ -0,0 +1,36 @@
+import csv
+import sys
+
+import numpy as np
+maxInt = sys.maxsize
+
+while True:
+    try:
+        csv.field_size_limit(maxInt)
+        break
+    except OverflowError:
+        maxInt = int(maxInt/10)
+
+data_dir = "../data/"
+embed_dir = "dictionary-mappings/"
+
+def get_word_embeddings(f_name) -> list[dict]:
+    ''' gets the pretrained embeddings from the csv
+        and converts it into a list of dictionaries
+        with keys "word" and "embeddings"
+    '''
+    
+    embed_list = []
+
+    with open(f_name, newline="") as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=",")
+        embed_list = list(reader)
+
+    for item in embed_list:
+        embed = item["embeddings"].replace("[", "").replace("]", "").replace(";", "\n")
+        new_array = np.fromstring(embed, sep="\n", dtype="float64")
+        item["embeddings"] = new_array
+        
+    return embed_list
+
+embeddings = get_word_embeddings(data_dir + embed_dir + "pretrained-embeddings.csv")
