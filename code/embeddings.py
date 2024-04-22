7aa2aa61623cde457a94c071fba673df28209915
Emily <efg12103@gmail.com>
2024-04-22 17:56:22 -0400

    edit embeddings to be stored more usefully

--- code/embeddings.py
+++ code/embeddings.py
@@ -15,10 +15,11 @@ while True:
 data_dir = "../data/"
 embed_dir = "dictionary-mappings/"
 
-def get_word_embeddings(f_name) -> list[dict]:
+def get_word_embeddings(f_name) -> dict:
     ''' gets the pretrained embeddings from the csv
-        and converts it into a list of dictionaries
-        with keys "word" and "embeddings"
+        and converts it into a dictionary
+        with the words as keys and the embeddings
+        as the value
     '''
     
     embed_list = []
@@ -27,11 +28,18 @@ def get_word_embeddings(f_name) -> list[dict]:
         reader = csv.DictReader(csvfile, delimiter=",")
         embed_list = list(reader)
 
+    new_dictionary = {}
+
     for item in embed_list:
         embed = item["embeddings"].replace("[", "").replace("]", "").replace(";", "\n")
         new_array = np.fromstring(embed, sep="\n", dtype="float64")
         item["embeddings"] = new_array
         
-    return embed_list
+        new_dictionary[item["word"]] = new_array
+        if item["word"] == "fawn":
+            print(new_dictionary["fawn"])
+        
+    return new_dictionary
 
 embeddings = get_word_embeddings(data_dir + embed_dir + "pretrained-embeddings.csv")
+#print(embeddings["fawn"])
