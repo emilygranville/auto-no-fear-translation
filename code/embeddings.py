d5dbb18c1969a4c5defd26d5f57e709310f465fa
Emily <efg12103@gmail.com>
2024-04-23 12:28:50 -0400

    edits gettings the embeddings based on the new embeddings file

--- code/embeddings.py
+++ code/embeddings.py
@@ -1,8 +1,12 @@
 import csv
 import sys
+import pandas as pd
 
 import numpy as np
 maxInt = sys.maxsize
+
+data_dir = "data/"
+embed_dir = "dictionary-mappings/short-embeddings/"
 
 # from https://stackoverflow.com/questions/15063936/csv-error-field-larger-than-field-limit-131072
 while True:
@@ -12,30 +16,47 @@ while True:
     except OverflowError:
         maxInt = int(maxInt/10)
 
-data_dir = "../data/"
-embed_dir = "dictionary-mappings/"
+EMBED_FILE_NAME_LIST = [
+    "short-embed01.csv", "short-embed02.csv", "short-embed03.csv",
+    "short-embed04.csv", "short-embed05.csv", "short-embed06.csv",
+    "short-embed07.csv", "short-embed08.csv", "short-embed09.csv",
+    "short-embed10.csv", "short-embed11.csv", "short-embed12.csv",
+    "short-embed13.csv", "short-embed14.csv", "short-embed15.csv",
+    "short-embed16.csv", "short-embed17.csv", "short-embed18.csv",
+    "short-embed19.csv", "short-embed20.csv", "short-embed21.csv",
+    "short-embed22.csv",
+]
 
-def get_word_embeddings(f_name) -> dict:
+small_list = [
+    "short-embed04.csv"
+]
+
+def get_word_embeddings(dir_name) -> dict:
     ''' gets the pretrained embeddings from the csv
         and converts it into a dictionary
         with the words as keys and the embeddings
         as the value
     '''
-    
-    embed_list = []
 
-    with open(f_name, newline="") as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=",")
-        embed_list = list(reader)
+    total_dictionary = {}
 
-    new_dictionary = {}
+    #for name in small_list:
+    for name in EMBED_FILE_NAME_LIST:
+        embed_list = []
 
-    for item in embed_list:
-        embed = item["embeddings"].replace("[", "").replace("]", "").replace(";", "\n")
-        new_array = np.fromstring(embed, sep="\n", dtype="float64")
+        with open((dir_name + name), newline="") as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=",")
+            embed_list = list(reader)
+
+        for item in embed_list:
+            try:
+                embed = item["embeddings"].replace("[", "").replace("]", "").replace(";", "\n")
+                new_array = np.fromstring(embed, sep="\n", dtype="float64")
+                total_dictionary[item["word"]] = new_array
+            except:
+                print("error in" + name + " item: " + str(item)[:20])
         
-        new_dictionary[item["word"]] = new_array
-        
-    return new_dictionary
+    return total_dictionary
 
-embeddings = get_word_embeddings(data_dir + embed_dir + "pretrained-embeddings.csv")
+embeddings = get_word_embeddings(data_dir + embed_dir)
+print("length: "+str(len(embeddings.keys())))
